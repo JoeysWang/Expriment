d1dfa845acca40d50735a5f63c9c51646d09f5a0
乔伊苏 <ky6115@163.com>
2019-05-21 10:13:27 +0800

    rxmodel store

--- app/src/main/java/com/joeys/expriment/MainActivity.kt
+++ app/src/main/java/com/joeys/expriment/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.joeys.expriment
 
 import android.os.Bundle
-import android.widget.EditText
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import com.joeys.analytics.Analytics
 import com.joeys.analytics.AnalyticsSettings
 import com.joeys.expriment.analytic.DemoAnalyticDispatcher
-import com.joeys.rxmodelstore.ModelStore
 import com.joeys.rxmodelstore.RxModelStore
-import java.util.*
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.atomic.AtomicBoolean
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,6 +35,8 @@ class MainActivity : AppCompatActivity() {
         false
     }
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -42,13 +46,71 @@ class MainActivity : AppCompatActivity() {
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
 
+
         val modelState = RxModelStore(UserEntity("joeys", 1))
         val d = modelState.modelState()
                 .subscribe {
+
                 }
 
         Analytics(AnalyticsSettings(), DemoAnalyticDispatcher(true))
 
 
+        val connectable = Observable.just("a")
+                .publish()
+                .refCount()
+        val dispose = connectable.replay(1)
+                .subscribe {
+
+                }
+
+
+//        connectable.connect()
+
+
     }
+
+    class RequstTask<T> private constructor() {
+
+
+        companion object {
+            private val INSTANCE: RequstTask<Any>
+                    by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED)
+                    {
+                        RequstTask<Any>()
+                    }
+
+            fun <T> requstQuene(request: Observable<T>) {
+                RequstTask<T>()
+
+            }
+
+        }
+    }
+
+    class RequstManager<T> {
+
+        private val isRequsting = AtomicBoolean(false)
+        private val hasValue = AtomicBoolean(false)
+        private val mWaitingPublisher = PublishSubject.create<T>()
+
+        fun request(observable: Observable<T>): Observable<T> {
+
+            if (!hasValue.get() && isRequsting.compareAndSet(false, true)) {
+                return observable.doOnNext {
+                    mWaitingPublisher.onNext(it)
+                    isRequsting.set(false)
+                }
+            } else {
+                return mWaitingPublisher
+                        .doOnNext {
+                            hasValue.set(true)
+                        }
+            }
+
+        }
+
+    }
+
+
 }
